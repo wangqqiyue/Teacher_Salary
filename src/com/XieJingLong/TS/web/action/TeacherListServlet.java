8e35f531141990b65f8be164f88cb0bf57c79b3d
wangqqiyue <1796483016@qq.com>
2021-09-30 11:54:16 +0800

    删除了Admin，清理了不必要的登录管理。 打算在下午完成一个工资项目管理。

--- src/com/XieJingLong/TS/web/action/TeacherListServlet.java
+++ src/com/XieJingLong/TS/web/action/TeacherListServlet.java
@@ -3,8 +3,10 @@ package com.XieJingLong.TS.web.action;
 import com.XieJingLong.TS.entity.Teacher;
 import com.XieJingLong.TS.util.DBUtil;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,47 +17,42 @@ import java.util.List;
 public class TeacherListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //验证用户是否已经登陆
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("Admin") == null) {
-            response.sendRedirect(request.getContextPath() + "/JSP/loginIntercept.jsp");
-        }
-        else {
-            Connection conn = null;
-            PreparedStatement ps = null;
-            ResultSet rs = null;
-            List<Teacher> teacherList = new ArrayList<>();
-            try {
-                conn = DBUtil.getConnection();
-                DBUtil.beginTransaction(conn);
-                String sql = "select id,realname,sex,post from tea_info";
-                ps = conn.prepareStatement(sql);
-                rs = ps.executeQuery();
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String realname = rs.getString("realname");
-                    String sex = rs.getString("sex");
-                    String post = rs.getString("post");
+        
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Teacher> teacherList = new ArrayList<>();
+        try {
+            conn = DBUtil.getConnection();
+            DBUtil.beginTransaction(conn);
+            String sql = "select id,realname,sex,post from tea_info";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String realname = rs.getString("realname");
+                String sex = rs.getString("sex");
+                String post = rs.getString("post");
 
-                    Teacher teacher = new Teacher();
-                    teacher.setId(id);
-                    teacher.setRealname(realname);
-                    teacher.setSex(sex);
-                    teacher.setPost(post);
+                Teacher teacher = new Teacher();
+                teacher.setId(id);
+                teacher.setRealname(realname);
+                teacher.setSex(sex);
+                teacher.setPost(post);
 
-                    teacherList.add(teacher);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                DBUtil.rollbackTransaction(conn);
-            } finally {
-                DBUtil.close(conn, ps, rs);
+                teacherList.add(teacher);
             }
-
-            //forward
-            request.setAttribute("teacherList", teacherList);
-            request.getRequestDispatcher("/JSP/list.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            DBUtil.rollbackTransaction(conn);
+        } finally {
+            DBUtil.close(conn, ps, rs);
         }
+
+        //forward
+        request.setAttribute("teacherList", teacherList);
+        request.getRequestDispatcher("/JSP/list.jsp").forward(request, response);
+
     }
     /*@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/JSP/failed.jsp
+++ web/JSP/failed.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
-  User: kinglong
-  Date: 2021/9/27
-  Time: 11:31
+  User: 17964
+  Date: 2021/9/30
+  Time: 11:49
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>错误信息</title>
 </head>
 <body>
-failed to operate,sorry...
+操作错误.
 </body>
 </html>
